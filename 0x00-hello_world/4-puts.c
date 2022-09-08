315d06f0a08933ae38f386770f4036860111ff28
Chiemelie10 <chiemelieeze@yahoo.com>
2022-09-08 19:56:12 +0000

    Solved task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * char str[] - Initializing strings
+ *
+ * puts (str) - Writing to stdout
+ *
+ * return: Always 0 (Success)
+ */
+int main()
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return (0);
+}
