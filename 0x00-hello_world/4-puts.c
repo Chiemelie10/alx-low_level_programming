5bd71ee079e159c5e9ce380a3b4e5ace1f29f2db
Chiemelie10 <chiemelieeze@yahoo.com>
2022-09-08 20:47:04 +0000

    Fixed error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,12 @@
 
 /**
  * main - Entry point
- *
- * char str[] - Initializing strings
- *
- * puts (str) - Writing to stdout
- *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
-	
+
 	puts(str);
 	return (0);
 }
