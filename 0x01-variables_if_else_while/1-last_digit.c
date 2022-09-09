13daccc48382303d160317c82a4b3e2cbab2a6fc
Chiemelie10 <chiemelieeze@yahoo.com>
2022-09-09 16:19:12 +0000

    Fixed declaration after statement error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,11 +9,10 @@
 int main(void)
 {
 	int n;
+	int last_dig;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_dig;
-
 	last_dig = n % 10;
 	if (last_dig > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_dig);
