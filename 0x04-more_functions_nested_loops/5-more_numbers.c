8d29c4e7d1c58b75383c18bd9a11bf8a372f44ba
Chiemelie10 <chiemelieeze@yahoo.com>
2022-09-16 11:59:29 +0000

    Prints the numbers from 0 to 14 using _putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - A function that prints 10 times the numbers
+ * from 0 to 14
+ * Return: Void
+ */
+
+void more_numbers(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 14; num2++)
+		{
+			if (num2 > 9)
+			{
+				_putchar(num2 / 10 + '0');
+				_putchar(num2 % 10 + '0');
+				continue;
+			}
+			_putchar(num2 + '0');
+		}
+		_putchar(10);
+	}
+}
