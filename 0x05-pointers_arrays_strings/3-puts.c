b200bc8253d95256755d9c1642c0edb8436a6f78
Chiemelie10 <chiemelieeze@yahoo.com>
2022-09-19 12:51:38 +0000

    Prints a sting using puts fuction

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - A funtion that prints a string, followed by a new line
+ *
+ * @str: Character array that contains the string
+ *
+ * Return: Void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
