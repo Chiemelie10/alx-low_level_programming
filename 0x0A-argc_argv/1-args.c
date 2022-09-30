7283ef6b56084a6ddb0d321db4a0126267b925e6
Chiemelie10 <chiemelieeze@yahoo.com>
2022-09-30 10:56:48 +0000

    Prints the number of command line arguements passed to it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: First arguement of main function
+ *
+ * @argv: Second arguement of main function
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
