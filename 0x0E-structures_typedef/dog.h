c9ec84dc0eb5e6eeee53bc67ea17bee38baf0609
Chiemelie10 <chiemelieeze@yahoo.com>
2022-10-10 23:28:52 +0000

    Created a file that new type "struct dog" with 3 members

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - A structure data type of dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the dog owner
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
