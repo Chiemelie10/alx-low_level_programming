1bcf5e234247b1b3827b4c68fdc10998f4ea0ace
Chiemelie10 <chiemelieeze@yahoo.com>
2022-10-11 14:45:58 +0000

    Defined a new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,10 +15,6 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - A new type dog_t as a new name for the type struct dog
- */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
