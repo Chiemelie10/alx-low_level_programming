d826dcfe8c4c20d3f2f6032efedc889ba56ec850
Chiemelie10 <chiemelieeze@yahoo.com>
2022-10-25 15:11:34 +0000

    Corrected an error in lists.h file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -10,12 +11,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
